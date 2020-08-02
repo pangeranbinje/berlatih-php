a3db5a7366dd600324185d292056fd3764b203db
pangeranbinje <49755249+pangeranbinje@users.noreply.github.com>
2020-08-02 23:59:12 +0700

    Update tentukan-deret-aritmatika.php

--- tentukan-deret-aritmatika.php
+++ tentukan-deret-aritmatika.php
@@ -13,7 +13,7 @@ function tentukan_deret_aritmatika($arr) {
 // TEST CASES
 echo tentukan_deret_aritmatika([1, 2, 3, 4, 5, 6]);// true
 echo tentukan_deret_aritmatika([2, 4, 6, 12, 24]);// false
-//echo tentukan_deret_aritmatika([2, 4, 6, 8]); //true
-//echo tentukan_deret_aritmatika([2, 6, 18, 54]);// false
-//echo tentukan_deret_aritmatika([1, 2, 3, 4, 7, 9]);// false
+echo tentukan_deret_aritmatika([2, 4, 6, 8]); //true
+echo tentukan_deret_aritmatika([2, 6, 18, 54]);// false
+echo tentukan_deret_aritmatika([1, 2, 3, 4, 7, 9]);// false
 ?>
